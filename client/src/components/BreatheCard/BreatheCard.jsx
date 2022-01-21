d6f481ed100745878d647d0e259b854a1681ca02
Enrique Barco <enrique.barco96@hotmail.com>
2022-01-21 11:55:33 -0600

    created stateful component and state change to show correct card

--- client/src/components/BreatheCard/BreatheCard.jsx
+++ client/src/components/BreatheCard/BreatheCard.jsx
@@ -3,15 +3,15 @@ import logo from "../../assets/images/logo.svg"
 import rightArrow from "../../assets/images/rightarrow.svg"
 import "./BreatheCard.scss"
 
-export default function BreatheCard() {
+export default function BreatheCard( {title, text}) {
   return(
       <div className="breathe-card">
           <div className="breath-card__content-container">
             <div className="breathe-card__title-container">
                 <img src={logo} alt="whoop logo" className="breathe-card__whoop-logo" />
-                <h1 className="breathe-card__title">Breathwork Challenge</h1>
+                <h1 className="breathe-card__title">{title}</h1>
             </div>
-            <p className="breathe-card__text">Often forgotten, but synchronized breathing can help you relax instantly!</p>
+            <p className="breathe-card__text">{text}</p>
           </div>
           <img src={rightArrow} alt="action icon" className="breathe-card__action-icon" />
       </div>
--- client/src/pages/HomePage/HomePage.js
+++ client/src/pages/HomePage/HomePage.js
@@ -1,17 +1,28 @@
 import './HomePage.scss';
+import { Component } from 'react';
 import Header from '../../components/Header/Header';
 import Navbar from '../../components/Navbar/Navbar.jsx';
 import ProgressBar from '../../components/Progress/ProgressBar';
+import BreatheCard from '../../components/BreatheCard/BreatheCard';
 
-function HomePage() {
-
-    return (
+export default class HomePage extends Component {
+    state = {
+        isBreatheCard: false,
+    }
+  render() {
+    return(
         <div>
             <Header />
             <Navbar />
             <ProgressBar />
+            {this.state.isBreatheCard 
+                ? <BreatheCard 
+                    title={"Breathwork Challenge"}
+                    text={"Often forgotten, but synchronized breathing can help you relax instantly!"}/> 
+                : <BreatheCard
+                    title={"Optimal Strain"}
+                    text={"You're at a balanced level of Strain for today. Going above 18.9 will promote fitness gains but may diminish your body’s ability to recover fully overnight. "}/>}
         </div>
-    )
-}
-
-export default HomePage;
+        )
+  }
+};
