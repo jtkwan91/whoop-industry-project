5af509c2bbeb674f999ac140f2f648d3d874ae5a
Enrique Barco <enrique.barco96@hotmail.com>
2022-01-21 10:00:25 -0600

    navbar complete

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom'
 import Header from './components/Header/Header';
+import Navbar from './components/Navbar/Navbar';
 
 export default class App extends Component {
   render() {
     return (
       <BrowserRouter>
       <Header />
+      <Navbar />
         <Switch>
           <Route />
           <Route />
---
+++ client/src/components/Navbar/Navbar.jsx
@@ -0,0 +1,21 @@
+import { NavLink } from "react-router-dom";
+import "./Navbar.scss";
+
+export default function Navbar() {
+  return (
+    <div className="navigation">
+        <NavLink to="/overview"  className="navigation__link">
+            <h3 className="navigation__title">overview</h3>
+        </NavLink>
+        <NavLink to="/strain" className="navigation__link">
+            <h3 className="navigation__title">strain</h3>
+        </NavLink>
+        <NavLink to="/recovery" className="navigation__link">
+            <h3 className="navigation__title">recovery</h3>
+        </NavLink>
+        <NavLink to="/sleep" className="navigation__link">
+            <h3 className="navigation__title">sleep</h3>
+        </NavLink>
+    </div>
+  )
+}
