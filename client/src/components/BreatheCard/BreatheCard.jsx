842303880378054c8ce80d792067027f9aad73eb
Enrique Barco <enrique.barco96@hotmail.com>
2022-01-21 13:33:50 -0600

    added new component for new card

--- client/src/components/BreatheCard/BreatheCard.jsx
+++ client/src/components/BreatheCard/BreatheCard.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
-import logo from "../../assets/images/logo.svg"
-import rightArrow from "../../assets/images/rightarrow.svg"
-import "./BreatheCard.scss"
+import logo from "../../assets/images/logo.svg";
+import leftArrow from "../../assets/images/leftarrow.svg";
+import "./BreatheCard.scss";
+import "animate.css";
 
 export default function BreatheCard( {title, text}) {
   return(
-      <div className="breathe-card">
+      <div className="breathe-card animate__animated animate__fadeInRight">
+          <img src={leftArrow} alt="action icon" className="breathe-card__action-icon" />
           <div className="breath-card__content-container">
             <div className="breathe-card__title-container">
                 <img src={logo} alt="whoop logo" className="breathe-card__whoop-logo" />
@@ -13,7 +15,6 @@ export default function BreatheCard( {title, text}) {
             </div>
             <p className="breathe-card__text">{text}</p>
           </div>
-          <img src={rightArrow} alt="action icon" className="breathe-card__action-icon" />
       </div>
     )
 }
--- client/src/pages/HomePage/HomePage.js
+++ client/src/pages/HomePage/HomePage.js
@@ -4,6 +4,7 @@ import Header from '../../components/Header/Header';
 import Navbar from '../../components/Navbar/Navbar.jsx';
 import ProgressBar from '../../components/Progress/ProgressBar';
 import BreatheCard from '../../components/BreatheCard/BreatheCard';
+import StrainCard from "../../components/StrainCard/StrainCard";
 
 export default class HomePage extends Component {
     state = {
@@ -19,7 +20,7 @@ export default class HomePage extends Component {
                 ? <BreatheCard 
                     title={"Breathwork Challenge"}
                     text={"Often forgotten, but synchronized breathing can help you relax instantly!"}/> 
-                : <BreatheCard
+                : <StrainCard
                     title={"Optimal Strain"}
                     text={"You're at a balanced level of Strain for today. Going above 18.9 will promote fitness gains but may diminish your body’s ability to recover fully overnight. "}/>}
         </div>
