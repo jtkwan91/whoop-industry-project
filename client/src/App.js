d1fa4850dedf7b0c0f050aeb3bac71c55c837f65
Joseph Kwan <jtkwan91@gmail.com>
2022-01-21 10:36:52 -0500

    basic breathing circle

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
 import { BrowserRouter, Switch, Route } from 'react-router-dom'
-import Header from './components/Header/Header';
+import BreathingCircle from './components/Header/BreathingCircle/BreathingCircle'
+import Header from './components/Header/Header'
 
 export default class App extends Component {
   render() {
@@ -8,7 +9,7 @@ export default class App extends Component {
       <BrowserRouter>
       <Header />
         <Switch>
-          <Route />
+          <Route path='/breathe' component={BreathingCircle}/>
           <Route />
         </Switch>
       </BrowserRouter>
---
+++ client/src/components/Header/BreathingCircle/BreathingCircle.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import './BreathingCircle.scss'
+
+function BreathingCircle() {
+  return <div className='breathe-circle'>Breathe Circle</div>;
+}
+
+export default BreathingCircle;
