fb650b4befca15f05d7371df44c7a9c2f80045d4
Joseph Kwan <jtkwan91@gmail.com>
2022-01-21 15:39:32 -0500

    made strain bar responsive

--- client/src/pages/HomePage/HomePage.js
+++ client/src/pages/HomePage/HomePage.js
@@ -2,26 +2,29 @@ import './HomePage.scss';
 import Header from '../../components/Header/Header';
 import Navbar from '../../components/Navbar/Navbar.jsx';
 import ProgressBar from '../../components/Progress/ProgressBar';
-import React, { useState } from 'react';
+import React from 'react';
 
 function HomePage() {
 
-    const [progress, setProgress] = useState(0);
-    const [color, setColor] = useState('');
-
-   
-    return (
+let strain = 16
+  
+       return (
        <>
             <Header />
             <Navbar />
             <ProgressBar 
-                progress={75}
+                progress={strain / 21 * 100
+                }
                 size={300}
                 strokeWidth={15}
                 circleOneStroke='#d9edfe'
                 />
             <h2 className="day-strain">DAY STRAIN</h2>
-            <h1 className="number">16.0</h1>
+            <div className="number-container">
+                <h1 className='number'>
+                {strain}
+                </h1>
+                </div>
             <div className="activity">
                 <p className="activity-text">1 ACTIVITY</p>
             </div>
