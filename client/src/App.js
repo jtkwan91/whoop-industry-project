a57a8c5af1e81a281f92bb2b708df8a4fd3bddfc
Joseph Kwan <jtkwan91@gmail.com>
2022-01-21 03:06:09 -0500

    header styled but not linked

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom'
+import Header from './components/Header/Header';
 
 export default class App extends Component {
   render() {
     return (
       <BrowserRouter>
-      
+      <Header />
         <Switch>
           <Route />
           <Route />
