39ea025df921f3dbf3775bac1c32af3edb05a5a7
Joseph Kwan <jtkwan91@gmail.com>
2022-01-21 13:30:42 -0500

    functional breathing animation and most styling done

--- client/src/pages/BreathingPage/BreathingPage.js
+++ client/src/pages/BreathingPage/BreathingPage.js
@@ -14,21 +14,24 @@ breatheState.setState('breathe__anim')
 
   return (
   <div className='breathe'>
-    <div className='breathe__header'>
-      <Link className='breathe__header-back' to='/'><img src={back} alt="backarrow" /></Link>
-      <h4 className='breathe__header-title'>BREATHWORK CHALLENGE</h4>
+    <div className='breathe__gradient'>
+      <div className='breathe__header'>
+        <Link className='breathe__header-back' to='/'><img src={back} alt="backarrow" /></Link>
+        <h4 className='breathe__header-title'>BREATHWORK CHALLENGE</h4>
+      </div>
+      <div className='breathe__challenge'>
+        <div className='breathe__circle breathe__anim' >Breathe Circle</div>
+        <button className='breathe__begin' onClick={handleClick}>Begin</button>
     </div>
-    <div className='breathe__challenge'>
-      <div className='breathe__circle' addclass={breatheState}>Breathe Circle</div>
-      <button className='breathe__begin' onClick={handleClick}>Begin</button>
+    </div>
       <div className='breathe__instructions'>
         <h4 className='breathe__instructions-title'>FOLLOW THE VISUALIZER</h4>
         <p className='breathe__instructions-details'>
-        Inhale slowly in through the nose, drawing the breath down toward the stomach. 
+        Inhale slowly in through the nose, drawing the breath down toward the stomach. <br /> <br />
         Tighten the abdominal muscles, letting the stomach fall downward while exhaling through the mouth. 
         </p>
         </div>
-      </div>
+
   </div>
   )
 }
