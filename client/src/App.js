afc3ed8e0f2b377520024f63a0a335eeb99d2a60
Joseph Kwan <jtkwan91@gmail.com>
2022-01-21 01:52:10 -0500

    header basic setup

--- client/src/App.js
+++ client/src/App.js
@@ -5,6 +5,7 @@ export default class App extends Component {
   render() {
     return (
       <BrowserRouter>
+      
         <Switch>
           <Route />
           <Route />
---
+++ client/src/components/Header/Header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import profile from '../../assets/images/profile.svg'
+
+function Header() {
+    return (
+      <div className='Header'>
+          <img src={profile} alt="" />
+
+
+      </div>
+)
+}
+
+export default Header;
